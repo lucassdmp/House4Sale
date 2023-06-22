f479122d8d8e8ca339c622607a809ba2988eefdf
Lucas <lucassdmp01@gmail.com>
2023-06-22 13:17:24 -0300

    component: housecard

---
+++ src/components/CatalogPage.tsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import HouseCard from "./HouseCard";
+
+function CatalogPage(){
+    return (
+        <Container className=" text-center">
+            <HouseCard/>
+        </Container>
+    )
+}
+
+export default CatalogPage;
